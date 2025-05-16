8da72b29c5d2b21d94bd391345937e02e04eda50
KenanAljaber <keno12333@hotmail.com>
2025-05-16 09:33:04 +0200

    hide navbar menu when clicking outside, install ag grid library

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import styles from "./Navbar.module.css";
 import { UserContext } from "../../../../context/userContext";
 import { useNavigate } from "react-router";
@@ -7,19 +7,32 @@ function Navbar() {
   const [user, setUser] = useState(null);
   const [dropdown, setDropdown] = useState(false);
   const navigate = useNavigate();
+  const wrapperRef = useRef(null);
 
   useEffect(() => {
     getUser().then((user) => {
       let initials = handleNameInitials(user.firstName); //TODO get full name
       user.initials = initials;
-      console.log(user);
+      
 
       setUser(user);
     });
   }, []);
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   function handleNameInitials(name) {
-    if (!name) return "USER";
+    if (!name) return "?";
     const words = name.split(" ");
     let initials = "";
     for (let i = 0; i < words.length; i++) {
@@ -40,6 +53,7 @@ function Navbar() {
         <div
           className={styles.userAvatar}
           onClick={() => setDropdown(!dropdown)}
+          ref={wrapperRef}
         >
           <p>{user?.initials}</p>
           {dropdown && (
