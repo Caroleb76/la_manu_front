28780fe9c4a35d7536ea3e7b3ee8f75ad0dfa144
Kenan Aljaber <keno12333@hotmail.com>
2025-06-18 10:02:42 +0200

    initial user grid implementation

--- src/components/DataGrid/DataGrid.jsx
+++ src/components/DataGrid/DataGrid.jsx
@@ -5,20 +5,42 @@ import usersHelper from "../../helpers/usersHelper";
 import styles from "./DataGrid.module.css";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
-const DataGrid = ({data,colDefs}) => {
- 
+const DataGrid = ({data,colDefs,actions}) => {
+  const [columnDefs, setColumnDefs] = useState([]);
+  // I should implement a way to add actions dynamically 
+  useEffect(() => {
+    let newDefs = [...colDefs];
+    
+    if(colDefs.filter((colDef) => colDef.field === "Actions").length>0){ 
+      
+      newDefs = newDefs.filter((colDef) => colDef.field !== "Actions");
+      newDefs.push(  {
+        headerName: "Actions",
+        field: "Actions",
+        filter: false,
+        cellRenderer: (params) => (
+          <div style={{ position: "relative" }}>
+            <p className={styles.actions} onClick={() => console.log(params.data)}>...</p>
+          </div>
+        ),
+      },)
+      
+      setColumnDefs(newDefs);
+    }
 
+  }, []);
 
   // Column Definitions: Defines the columns to be displayed.
 
   return (
 <div className={styles.customAgGrid} style={{
-     height: 500, width: "100%",
+     height: "90%", width: "100%",
       }}>
   <AgGridReact
     rowData={data}
-    columnDefs={colDefs}
+    columnDefs={columnDefs}
     pagination={true}
+    paginationPageSize={12}
     defaultColDef={{
       flex: 1,
       filter: true,
--- src/views/Users/users.jsx
+++ src/views/Users/users.jsx
@@ -1,36 +1,40 @@
 import { useEffect, useState } from "react";
 import DataGrid from "../../components/DataGrid/DataGrid";
 import usersHelper from "../../helpers/usersHelper";
+import Styles from "./User.module.css";
 
 function Users() {
-    const [users, setUsers] = useState([]);
-    const colDefs =   [{ field: "Email", filter: true },
-        { field: "Nom", filter: true },
-        { field: "Prenom", filter: true },
-        { field: "Role", filter: true },
-        { field: "Actions", filter: false }];
-    useEffect(() => {
-      async function getUsers() {
-        const response = await usersHelper.getUsers();
-        const responseUsers = response.data;
-        console.log(responseUsers);
-  
-        setUsers(
-          responseUsers.map((user) => ({
-            Nom: user.lastName,
-            Prenom: user.firstName,
-            Email: user.email,
-            Role: user.role.name,
-          }))
-        );
-      }
-      getUsers();
-    }, []);
-    return ( 
-        <>
-        <DataGrid colDefs={colDefs} data={users}/>
-        </>
-     );
+  const [users, setUsers] = useState([]);
+  const colDefs = [
+    { field: "Email", filter: true },
+    { field: "Nom", filter: true },
+    { field: "Prenom", filter: true },
+    { field: "Role", filter: true },
+    { field: "Actions", filter: false },
+  ];
+  useEffect(() => {
+    async function getUsers() {
+      const response = await usersHelper.getUsers();
+      const responseUsers = response.data;
+      console.log(responseUsers);
+
+      setUsers(
+        responseUsers.map((user) => ({
+          Nom: user.lastName,
+          Prenom: user.firstName,
+          Email: user.email,
+          Role: user.role.name,
+        }))
+      );
+    }
+    getUsers();
+  }, []);
+  return (
+    <div className={Styles.mainContainer}>
+      <button className={Styles.addButton}>Créer</button>
+      <DataGrid colDefs={colDefs} data={users} />
+    </div>
+  );
 }
 
 export default Users;
