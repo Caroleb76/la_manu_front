c26f30d75d3598d3f132f20ab0b9339200525be3
Kenan Aljaber <keno12333@hotmail.com>
2025-06-19 11:56:52 +0200

    notification implementation

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -4,13 +4,16 @@ import styles from "./Dashboard.module.css";
 import { Outlet, useLocation, useNavigate } from "react-router";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../../context/userContext";
+import { useNotification } from "../../../context/notificationContext";
 function Dashboard() {
   const {getUser}=useContext(UserContext);
   const navigate= useNavigate();
+  const {notify}= useNotification();
   useEffect(() => {
     getUser().then(user => {
       if (!user) navigate("/login");
     });
+
   }, [location]);
   
 
