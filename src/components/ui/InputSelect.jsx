cf031475fb9483a561b15fffb0f3d46fbe16d71a
loic grancher <loic.grancher@protonmail.com>
2025-06-19 09:28:51 +0200

    added react hook form and zod for validation

--- src/components/ui/InputSelect.jsx
+++ src/components/ui/InputSelect.jsx
@@ -1,17 +1,29 @@
-
-export default function InputSelect({label, defaultValue, value, name, onChange, children, error, ...etc}) {
-  return (
-    <div>
+export default function InputSelect({
+    label,
+    defaultValue,
+    value,
+    name,
+    onChange,
+    children,
+    error,
+    ...etc
+}) {
+    return (
+        <div>
             <label htmlFor={name}>{label}</label>
-            <select name={name} value={value} defaultValue={defaultValue ? defaultValue : "default"} onChange={onChange} {...etc}>
-              <option value="default" disabled>
-                Sélectionner une option
-              </option>
-           {children}
+            <select
+                name={name}
+                value={value}
+                defaultValue={defaultValue ? defaultValue : "default"}
+                onChange={onChange}
+                {...etc}
+            >
+                <option value="default" disabled>
+                    Sélectionner une option
+                </option>
+                {children}
             </select>
-            <p className="inputError">
-          {error}
-        </p>
-          </div>
-  )
+            <p className="inputError">{error}</p>
+        </div>
+    );
 }
