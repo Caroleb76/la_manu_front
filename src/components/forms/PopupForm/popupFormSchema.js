abbd9def1274cd85b350912945158dce10e93325
Caroleb76 <BANVILLE@agifen.local>
2025-06-20 15:34:44 +0200

    popup notification

---
+++ src/components/forms/PopupForm/popupFormSchema.js
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+export const popupFormSchema = z.object({
+
+
+
+  firstName: z
+    .string()
+    .min(2, 'Le titre doit contenir au moins 2 caractères.')
+    .max(50, 'Le titre est trop long.'),
+
+  priority: z
+    .coerce.number()
+    .min(1, 'Une priorité est obligatoire')
+    .max(3)
+    .nonnegative(),
+
+
+  startDate: z
+    .string()
+    .min(1, 'La date de publication est requise.'),
+
+  endDate: z
+  .string()
+  .min(1, 'La date de fin est requise.'),
+
+
+  content: z
+    .string()
+    .min(2, 'Le contenu doit contenir au moins 2 caractères.')
+    .max(250, 'Le contenu est trop long.'),
+
+
+});
