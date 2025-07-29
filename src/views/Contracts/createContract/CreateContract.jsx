538c83f555a4011376bca129da7eddd2a7607310
Loic <loic.grancher@protonmail.com>
2025-07-29 09:32:10 +0200

    fixed createContract import bug

--- src/views/Contracts/createContract/CreateContract.jsx
+++ src/views/Contracts/createContract/CreateContract.jsx
@@ -1,41 +1,41 @@
-
-import { useState, useEffect, useMemo } from "react";
+import { useState} from "react";
 
 import notificationsHelper from "../../../helpers/notificationsHelper";
 import styles from "./CreateContract.module.css";
 import PopupWrapper from "../../../components/popups/PopupWrapper.jsx";
 import { useNotification } from "../../../../context/notificationContext";
 import ContractCreateForm from "../../../components/forms/ContractCreateForm/ContractCreateForm.jsx";
+import PopupformNotification from "../../../components/forms/PopupFormNotification/PopupformNotification.jsx";
 
 export default function CreateContract() {
-    const [interventionCreationMode, setInterventionCreationMode] = useState(false);
+    const [interventionCreationMode, setInterventionCreationMode] =
+        useState(false);
     const { notify } = useNotification();
 
     const [reloadTrigger, setReloadTrigger] = useState(0);
     const [searchText, setSearchText] = useState("");
-
 
     const onSearchTextChange = (e) => {
         setSearchText(e.target.value);
         setReloadTrigger(reloadTrigger + 1);
     };
     const onDeleteNotification = async (notification) => {
-
-
-        const response = await notificationsHelper.deleteNotification(notification.id);
+        const response = await notificationsHelper.deleteNotification(
+            notification.id
+        );
         if (response && response.success) {
-            setReloadTrigger(prev => prev + 1);
+            setReloadTrigger((prev) => prev + 1);
             notify("La notification a bien été supprimée", "success");
         } else {
             notify("Une erreur est survenue", "error");
         }
-    }
+    };
 
-    const onInterventionAdd = () => {
+    const onInterventionCreated = () => {
         setInterventionCreationMode(false);
-        setReloadTrigger(prev => prev + 1);
+        setReloadTrigger((prev) => prev + 1);
         notify("La notification a bien été ajoutée", "success");
-    }
+    };
 
     return (
         <>
@@ -52,17 +52,11 @@ export default function CreateContract() {
                         </PopupWrapper>
                     </>
                 )}
-                <ContractCreateForm onSessionCreated={() => { }} showPopup={setInterventionCreationMode(true)} />
-
-
+                <ContractCreateForm
+                    onSessionCreated={() => {}}
+                    showPopup={setInterventionCreationMode(true)}
+                />
             </div>
-
         </>
     );
 }
-
-
-
-
-
-
